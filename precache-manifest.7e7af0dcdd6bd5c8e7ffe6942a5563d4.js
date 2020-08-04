00d2a4b8a1b48665d851f920f5421fe453391ccc
Benyamin <benyamin.es79@gmail.com>
2020-08-04 19:31:49 +0430

    Updates

---
+++ precache-manifest.7e7af0dcdd6bd5c8e7ffe6942a5563d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95915ce591aaf6e50e4d68b0c9af1ca",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "ed4945c9e0e71e199afe",
+    "url": "/react-resume/static/css/main.fdc8c070.chunk.css"
+  },
+  {
+    "revision": "d070d34d5c9939bf586a",
+    "url": "/react-resume/static/js/2.d250ee7c.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/react-resume/static/js/2.d250ee7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4945c9e0e71e199afe",
+    "url": "/react-resume/static/js/main.a2c4e6ac.chunk.js"
+  },
+  {
+    "revision": "42554a48dd7e666c6cd2",
+    "url": "/react-resume/static/js/runtime-main.dc160fd6.js"
+  }
+]);
